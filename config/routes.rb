102f0d083e3ff587995e21ac620fa6e977044544
Stefan VonStein <sjvonstein@gmail.com>
2021-02-10 20:52:44 -0500

    added search to index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   #get 'home/index'
   get 'home/about'
   root 'home#index'
+
+  get '/search' => 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
